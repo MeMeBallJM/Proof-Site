dbd8915e2408b2f72e5fe3b252c50aa3254935db
MeMeBall <91382906+MeMeBallJM@users.noreply.github.com>
2024-02-27 19:25:43 -0500

    Added logging to data routing function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use axum::response::Html;
 use axum::routing::get;
 use std::net::SocketAddr;
+use tracing::{span, Level};
 
 mod utils;
 use utils::data_routing;
 
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new()
-        .route("/", get(handler))
-        .route("/client/:file_name", get(data_routing::file_pool));
+    tracing_subscriber::fmt::init();
 
+    let app = axum::Router::new().route("/client/:file", get(data_routing::fetch_file));
+
+    let span = span!(Level::INFO, "server");
+    let _enter = span.enter();
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     println!("Server started, listening on {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .expect("Failed to start server");
-}
-
-async fn handler() -> Html<&'static str> {
-    Html(include_str!("./client/index.html"))
+    drop(_enter);
 }
